f219a694455a94e974e71cd8cd83b5baba0fb11b
Jérémy LANOUE <jlanoue.18@gmail.com>
2023-05-15 17:34:58 +0200

    initialisation ArticleVenduDAO

---
+++ src/main/java/fr/eni/dal/jdbc/ArticleVenduDAOJdbcImpl.java
@@ -0,0 +1,59 @@
+package fr.eni.dal.jdbc;
+
+import java.util.List;
+
+import fr.eni.projet.dal.ArticleVendu;
+import fr.eni.projet.dal.ArticleVenduDALException;
+import fr.eni.projet.dal.ArticleVenduDAO;
+
+public class ArticleVenduDAOJdbcImpl implements ArticleVenduDAO {
+
+	@Override
+	public ArticleVendu selectById(int pArticleVenduId) throws ArticleVenduDALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ArticleVendu> selectAll() throws ArticleVenduDALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArticleVendu selectArticleByUtilisateur(int pUtilisateurId) throws ArticleVenduDALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ArticleVendu> selectArticleVenduByUtilisateur(int pUtilisateurId) throws ArticleVenduDALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insererArticleVendu(ArticleVendu pArticleVendu) throws ArticleVenduDALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void supprimerArticleVendu(int pArticleVenduId) throws ArticleVenduDALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ArticleVendu> selectionnerParCategorie(String pLibelleCategorie) throws ArticleVenduDALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insererArticleVendu(ArticleVendu pArticleVendu) throws ArticleVenduDALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
