68ee4641bfc2746b8fb788303088fcb4b283a4d0
Jérémy LANOUE <jlanoue.18@gmail.com>
2023-05-15 15:48:47 +0200

    Create DAOFactory.java

---
+++ src/main/java/fr/eni/projet/dal/DAOFactory.java
@@ -0,0 +1,34 @@
+package fr.eni.projet.dal;
+
+import fr.eni.dal.jdbc.ArticleVenduDAOJdbcImpl;
+import fr.eni.dal.jdbc.EnchereDAOJdbcImpl;
+import fr.eni.dal.jdbc.UtilisateurDAOJdbcImpl;
+
+public abstract class DAOFactory {
+	/**
+	 * recuperer l'instance de UtilisateurDAOJdbcImpl
+	 * 
+	 * @return
+	 */
+	public static UtilisateurDAO recupererUtilisateurDAO() {
+		return new UtilisateurDAOJdbcImpl();
+	}
+	
+	/**
+	 * recuperer l'instance de ArticleVenduDAOJdbcImpl
+	 * 
+	 * @return
+	 */
+	public static ArticleVenduDAO recupererArticleVenduDAO() {
+		return new ArticleVenduDAOJdbcImpl();
+	}
+	
+	/**
+	 * recuperer l'instance de EnchereDAOJdbcImpl
+	 * 
+	 * @return
+	 */
+	public static EnchereDAO recupererEnchereDAO() {
+		return new EnchereDAOJdbcImpl();
+	}
+}
