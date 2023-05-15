43e10c6b9e058ea2e4234cf6b84b21836fd571ba
Jérémy LANOUE <jlanoue.18@gmail.com>
2023-05-15 15:23:13 +0200

    Create UtilisateurDAO.java

---
+++ src/main/java/fr/eni/projet/dal/UtilisateurDAO.java
@@ -0,0 +1,55 @@
+package fr.eni.projet.dal;
+
+import java.util.List;
+
+public interface UtilisateurDAO {
+	/**
+	 * selectionner un utilisateur par son id
+	 * 
+	 * @param pUtilisateurId
+	 * @return
+	 * @throws UtilisateurDALException
+	 */
+	public Utilisateur selectById(int pUtilisateurId) throws UtilisateurDALException;
+	
+	/**
+	 * selectionner tous les utilisateurs
+	 * 
+	 * @return
+	 * @throws UtilisateurDALException
+	 */
+	public List<Utilisateur> selectAll() throws UtilisateurDALException;
+	
+	/**
+	 * inserer un nouvel utilisateur
+	 * 
+	 * @param pUtilisateur
+	 * @throws UtilisateurDALException
+	 */
+	public void insererUtilisateur(Utilisateur pUtilisateur) throws UtilisateurDALException;
+	
+	/**
+	 * supprimer un utilisateur
+	 * 
+	 * @param pUtilisateurId
+	 * @throws UtilisateurDALException
+	 */
+	public void supprimerUtilisateur(int pUtilisateurId) throws UtilisateurDALException;
+	
+	/**
+	 * desactiver un utilisateur
+	 * 
+	 * @param pUtilisateurId
+	 * @throws UtilisateurDALException
+	 */
+	public void desactiverUtilisateur(int pUtilisateurId) throws UtilisateurDALException;
+	
+	/**
+	 * modifier un utilisateur
+	 * 
+	 * @param pUtilisateur
+	 * @throws UtilisateurDALException
+	 */
+	public void modifierUtilisateur(Utilisateur pUtilisateur) throws UtilisateurDALException;
+	
+}
