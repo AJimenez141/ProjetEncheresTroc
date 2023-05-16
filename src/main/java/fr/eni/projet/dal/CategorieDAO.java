bb3a1fd725f776a09b4c24cfef5681673c8bce8f
AJimenez141 <102180111+AJimenez141@users.noreply.github.com>
2023-05-16 15:59:56 +0200

    CategoriesDAO ajouts

--- src/main/java/fr/eni/projet/dal/CategorieDAO.java
+++ src/main/java/fr/eni/projet/dal/CategorieDAO.java
@@ -1,5 +1,59 @@
 package fr.eni.projet.dal;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import fr.eni.projet.bo.Categorie;
+
 public interface CategorieDAO {
+	
+	/**
+	 * selectionner une categorie par son id
+	 * 
+	 * @param pUtilisateurId
+	 * @return
+	 * @throws UtilisateurDALException
+	 * @throws CategorieDALException 
+	 */
+	public Categorie selectById(int pCategorieId) throws CategorieDALException;
+	
+	/**
+	 * selectionner toutes les categories
+	 * 
+	 * @return List<Categorie>
+	 * @throws UtilisateurDALException
+	 * @throws CategorieDALException 
+	 */
+	public List<Categorie> SelectAll() throws CategorieDALException;
+	
+	/**
+	 * inserer une nouvelle categorie
+	 * 
+	 * @param pCategorie
+	 * @return
+	 * @throws UtilisateurDALException
+	 * @throws CategorieDALException 
+	 */
+	public void insererCategorie(Categorie pCategorie) throws CategorieDALException;
+	
+	/**
+	 * supprimer une categorie
+	 * 
+	 * @param pCategorieId
+	 * @return
+	 * @throws UtilisateurDALException
+	 * @throws CategorieDALException 
+	 */
+	public void supprimerCategorie(int pCategorieId) throws CategorieDALException;
+	
+	/**
+	 * modifier une categorie
+	 * 
+	 * @param pCategorieId
+	 * @return
+	 * @throws UtilisateurDALException
+	 * @throws CategorieDALException 
+	 */
+	public void modifierCategorie(int pCategorieId) throws CategorieDALException;
 
 }
--- src/main/java/fr/eni/projet/dal/CategorieDAOException.java
+++
@@ -1,5 +0,0 @@
-package fr.eni.projet.dal;
-
-public class CategorieDAOException {
-
-}
