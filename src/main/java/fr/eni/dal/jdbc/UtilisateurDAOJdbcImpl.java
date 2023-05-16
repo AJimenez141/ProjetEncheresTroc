7a1b3abfdcf2bfd5c0aa8bd2fa6b85fbfafdee48
Jérémy LANOUE <jlanoue.18@gmail.com>
2023-05-16 10:47:51 +0200

    Update UtilisateurDAOJdbcImpl.java
    Complétion des methodes

--- src/main/java/fr/eni/dal/jdbc/UtilisateurDAOJdbcImpl.java
+++ src/main/java/fr/eni/dal/jdbc/UtilisateurDAOJdbcImpl.java
@@ -10,8 +10,10 @@ import java.util.List;
 
 import fr.eni.projet.bo.Utilisateur;
 import fr.eni.projet.dal.ConnectionProvider;
+import fr.eni.projet.dal.ConnexionException;
 import fr.eni.projet.dal.UtilisateurDALException;
 import fr.eni.projet.dal.UtilisateurDAO;
+import fr.eni.projet.bo.Adresse;
 
 public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
 	
@@ -20,17 +22,39 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
 	private final String INSERT_UTILISATEUR		= "INSERT INTO UTILISATEUR(no_utilisateur,pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur) VALUES (?,?,?,?,?,?,?,?,?,0,0,0)";
 	private final String DELETE_UTILISATEUR		= "DELETE FROM UTILISATEUR WHERE noUtilisateur = ?";
 	private final String UPDATE_UTILISATEUR		= "UPDATE UTILISATEUR SET  WHERE noUtilisateur = ?";
-	private final String MODIFIER_CREDIT			= "UPDATE UTILISATEUR SET credit = ? WHERE noUtilisateur = ?";
+	private final String CONNEXION_UTILISATEUR_EMAIL	= "SELECT * FROM UTILISATEUR WHERE email = ? AND mot_de_passe = ?";
+	private final String CONNEXION_UTILISATEUR_PSEUDO	= "SELECT * FROM UTILISATEUR WHERE pseudo = ? AND mot_de_passe = ?";
 
 	@Override
-	public Utilisateur selectById(int pUtilisateurId) throws UtilisateurDALException {
+	public Utilisateur selectById(int pUtilisateurId) throws UtilisateurDALException, SQLException {
+		Utilisateur utilisateur = null;
+		
 		try(
 			Connection connexion = ConnectionProvider.getConnection();
 			PreparedStatement pStmt = connexion.prepareStatement(SELECT_UTILISATEUR_BY_ID);
 		){
 			
+			ResultSet rs = pStmt.executeQuery();
+			
+			if(rs.next()) {
+				int noUtilisateur  	= rs.getInt("no_utilisateur");
+				String pseudo		= rs.getString("pseudo");
+				String nom			= rs.getString("nom");
+				String prenom		= rs.getString("prenom");
+				String email		= rs.getString("email");
+				String telephone	= rs.getString("telephone");
+				String rue			= rs.getString("rue");
+				String code_postal	= rs.getString("code_postal");
+				String ville		= rs.getString("ville");
+				Double credit		= rs.getDouble("credit");
+				
+				Adresse adresse 	= new Adresse(rue,code_postal,ville);
+				
+				utilisateur = new Utilisateur(noUtilisateur,pseudo,nom,prenom,email,telephone,adresse,credit);
+			}
+			
 		}
-		return null;
+		return utilisateur;
 	}
 
 	/**
@@ -59,6 +83,7 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
 				Double credit		= rs.getDouble("credit");
 				
 				Adresse adresse 	= new Adresse(rue,code_postal,ville);
+				
 				utilisateurs.add(new Utilisateur(noUtilisateur,pseudo,nom,prenom,email,telephone,adresse,credit));
 			}
 		} catch (SQLException e) {
@@ -81,7 +106,7 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
 			pStmt.setString(2, pUtilisateur.getNom());
 			pStmt.setString(3, pUtilisateur.getPrenom());
 			pStmt.setString(4, pUtilisateur.getEmail());
-			pStmt.setString(5, pUtilisateur.getMotDePasseHache);
+			pStmt.setString(5, pUtilisateur.getMotDePasseHashe());
 			pStmt.setString(6, pUtilisateur.getTelephone());
 			pStmt.setString(7, adresseUtilisateur.getRue());
 			pStmt.setString(8, adresseUtilisateur.getCodePostal());
@@ -100,29 +125,109 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
 	}
 
 	@Override
-	public void insererUtilisateur(Utilisateur pUtilisateur) throws UtilisateurDALException {
-		// TODO Auto-generated method stub
+	public void supprimerUtilisateur(int pUtilisateurId) throws UtilisateurDALException, SQLException {
+		try(
+			Connection connexion = ConnectionProvider.getConnection();
+			PreparedStatement pStmt = connexion.prepareStatement(DELETE_UTILISATEUR);
+		){
+			pStmt.setInt(pUtilisateurId, pUtilisateurId);
+			pStmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new UtilisateurDALException("Une erreur est survenue lors de la suppression de l'utilisateur");
+		}
 		
 	}
 
 	@Override
-	public void supprimerUtilisateur(int pUtilisateurId) throws UtilisateurDALException {
-		// TODO Auto-generated method stub
-		
+	public void modifierUtilisateur(Utilisateur pUtilisateur) throws UtilisateurDALException, SQLException {
+		try(
+			Connection connexion = ConnectionProvider.getConnection();	
+			PreparedStatement pStmt = connexion.prepareStatement(UPDATE_UTILISATEUR)
+		){
+			Adresse adresseUtilisateur = pUtilisateur.getAdresse();
+			
+			pStmt.setInt(1, pUtilisateur.getNoUtilisateur());
+			pStmt.setString(2, pUtilisateur.getPseudo());
+			pStmt.setString(3, pUtilisateur.getNom());
+			pStmt.setString(4, pUtilisateur.getPrenom());
+			pStmt.setString(5, pUtilisateur.getEmail());
+			pStmt.setString(6, pUtilisateur.getMotDePasseHashe());
+			pStmt.setString(7, pUtilisateur.getTelephone());
+			pStmt.setString(8, adresseUtilisateur.getRue());
+			pStmt.setString(9, adresseUtilisateur.getCodePostal());
+			pStmt.setString(10, adresseUtilisateur.getVille());
+			pStmt.setDouble(11, pUtilisateur.getCredit());
+			pStmt.setBoolean(12, false);
+			
+			pStmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new UtilisateurDALException("Impossible de mettre à jour l'utilisateur " + pUtilisateur.getPseudo());
+		}		
 	}
 
 	@Override
-	public void modifierUtilisateur(Utilisateur pUtilisateur) throws UtilisateurDALException {
-		// TODO Auto-generated method stub
+	public Utilisateur seConnecter(String pEmailOrPseudo, String pMotDePasse)
+			throws UtilisateurDALException, SQLException, ConnexionException {
 		
-	}
-
-	@Override
-	public void modifierCredit(int pUtilisateurId) throws UtilisateurDALException {
-		// TODO Auto-generated method stub
+		Utilisateur resultat = null;
 		
+		try(
+			Connection connexion = ConnectionProvider.getConnection();	
+			PreparedStatement pStmt = connexion.prepareStatement(CONNEXION_UTILISATEUR_EMAIL);
+			PreparedStatement pStmt2 = connexion.prepareStatement(CONNEXION_UTILISATEUR_PSEUDO);
+		){
+			pStmt.setString(1, pEmailOrPseudo);
+			pStmt.setString(2, pMotDePasse);
+			
+			ResultSet rs = pStmt.executeQuery();
+			
+			if(rs.next()) {
+				resultat = new Utilisateur(
+					rs.getInt("no_utilisateur"),
+					rs.getString("pseudo"),
+					rs.getString("nom"),
+					rs.getString("prenom"),
+					rs.getString("email"),
+					rs.getString("telephone"),
+					new Adresse(
+						rs.getString("rue"),
+						rs.getString("code_postal"),
+						rs.getString("ville")
+					),
+					rs.getDouble("credit"),
+					rs.getInt("administrateur")
+				);				
+			}
+			
+			pStmt2.setString(1, pEmailOrPseudo);
+			pStmt2.setString(2, pMotDePasse);
+			
+			ResultSet rs2 = pStmt2.executeQuery();
+			
+			if(rs2.next()) {
+				resultat = new Utilisateur(
+					rs.getInt("no_utilisateur"),
+					rs.getString("pseudo"),
+					rs.getString("nom"),
+					rs.getString("prenom"),
+					rs.getString("email"),
+					rs.getString("telephone"),
+					new Adresse(
+						rs.getString("rue"),
+						rs.getString("code_postal"),
+						rs.getString("ville")
+					),
+					rs.getDouble("credit"),
+					rs.getInt("administrateur")
+				);				
+			}
+			
+		}catch (SQLException e) {
+			e.printStackTrace();
+			throw new ConnexionException("Impossible de se connecter, vérifier les informations de connexion ");
+		}		
+		return resultat;
 	}
-
-
-
 }
