34322e758801f27183cc38e8c3d73f4e8d526082
Jérémy LANOUE <jlanoue.18@gmail.com>
2023-05-17 10:31:14 +0200

    UtilisateurManager complet

--- src/main/java/fr/eni/projet/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/projet/bll/UtilisateurManager.java
@@ -1,5 +1,6 @@
 package fr.eni.projet.bll;
 
+import fr.eni.projet.dal.ConnexionException;
 import fr.eni.projet.dal.DAOFactory;
 import fr.eni.projet.dal.UtilisateurDALException;
 import fr.eni.projet.dal.UtilisateurDAO;
@@ -13,15 +14,89 @@ public class UtilisateurManager {
 
 	private UtilisateurDAO utilisateurDAO;
 	
+	/**
+	 * Recuperer utilisateurDAO via la Factory
+	 */
 	public UtilisateurManager() {
 		this.utilisateurDAO = DAOFactory.recupererUtilisateurDAO();
 	}
 	
-	public List<Utilisateur> recupererUtilisateurs() throws UtilisateurDALException {
+	/**
+	 * Recuperation de tous les utilisateurs
+	 * 
+	 * @return List<Utilisateur>
+	 * @throws UtilisateurDALException
+	 */
+	public List<Utilisateur> recupererLesUtilisateurs() throws UtilisateurDALException {
 		return this.utilisateurDAO.selectAll();
 	}
 	
-	public Utilisateur selectionnerUtilisateur(int pIdUtilisateur) throws UtilisateurDALException, SQLException {
+	/**
+	 * Recuperation d'un utilisateur via son ID
+	 * 
+	 * @param pIdUtilisateur
+	 * @return Utilisateur
+	 * @throws UtilisateurDALException
+	 * @throws SQLException
+	 */
+	public Utilisateur recupererUnUtilisateur(int pIdUtilisateur) throws UtilisateurDALException, SQLException {
 		return this.utilisateurDAO.selectById(pIdUtilisateur);
+	}
+	
+	/**
+	 * Insertion d'un utilisateur dans la BDD
+	 * 
+	 * TODO - effectuer des controles avant mis en bdd
+	 * 
+	 * @param pUtilisateur
+	 * @throws UtilisateurDALException
+	 */
+	public void insererUtilisateur(Utilisateur pUtilisateur) throws UtilisateurDALException {
+		this.utilisateurDAO.insererUtilisateur(pUtilisateur);
+	}
+	
+	/**
+	 * Modification d'un utilisateur
+	 * 
+	 * TODO - effectuer des controles avant modification dans la bdd
+	 * 
+	 * @param pUtilisateur
+	 * @throws UtilisateurDALException
+	 * @throws SQLException
+	 */
+	public void modifierUtilisateur(Utilisateur pUtilisateur) throws UtilisateurDALException, SQLException {
+		this.utilisateurDAO.modifierUtilisateur(pUtilisateur);
+	}
+	
+	/**
+	 * Suppression d'un utilisateur
+	 * 
+	 * TODO - NE PAS OUBLIER DE DEMANDER UNE CONFIRMATION
+	 * AU PREALABLE
+	 * 
+	 * @param pUtilisateurId
+	 * @throws UtilisateurDALException
+	 * @throws SQLException
+	 */
+	public void supprimerUtilisateur(int pUtilisateurId) throws UtilisateurDALException, SQLException {
+		this.utilisateurDAO.supprimerUtilisateur(pUtilisateurId);
+	}
+	
+	/**
+	 * Recuperer l'utilisateur au moment de la connexion
+	 * 
+	 * TODO - faire les controles au moment de la connexion
+	 * TODO - methode dans le DAOJdbcImpl possiblement à revoir,
+	 * ajout d'un token en front ?
+	 * 
+	 * @param pEmailOrLogin
+	 * @param pMotDePasse
+	 * @return Utilisateur
+	 * @throws UtilisateurDALException
+	 * @throws SQLException
+	 * @throws ConnexionException
+	 */
+	public Utilisateur seConnecter(String pEmailOrLogin, String pMotDePasse) throws UtilisateurDALException, SQLException, ConnexionException {
+		return this.utilisateurDAO.seConnecter(pEmailOrLogin, pMotDePasse);
 	}
 }
