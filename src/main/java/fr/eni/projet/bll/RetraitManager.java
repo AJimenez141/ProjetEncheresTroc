8fa3fb7bcd67f741101e8dd6c8a72bcbe95e6a35
Jérémy LANOUE <jlanoue.18@gmail.com>
2023-05-17 11:18:46 +0200

    Update RetraitManager.java

--- src/main/java/fr/eni/projet/bll/RetraitManager.java
+++ src/main/java/fr/eni/projet/bll/RetraitManager.java
@@ -1,5 +1,34 @@
 package fr.eni.projet.bll;
 
-public class RetraitManager {
+import java.sql.SQLException;
 
+import fr.eni.projet.bo.Retrait;
+import fr.eni.projet.dal.DALException;
+import fr.eni.projet.dal.DAOFactory;
+import fr.eni.projet.dal.RetraitDALException;
+import fr.eni.projet.dal.RetraitDAO;
+
+public class RetraitManager {
+	private RetraitDAO retraitDAO;
+	
+	/**
+	 * Recupere RetraitDAO via la Factory
+	 */
+	public RetraitManager() {
+		this.retraitDAO = DAOFactory.recupererRetraitDAO();
+	}
+	
+	/**
+	 * Creation d'un retrait
+	 * 
+	 * TODO - faire des verification avant l'ajout en BDD
+	 * 
+	 * @param pRetrait
+	 * @throws RetraitDALException
+	 * @throws DALException
+	 * @throws SQLException
+	 */
+	public void creerRetrait(Retrait pRetrait) throws RetraitDALException, DALException, SQLException {
+		this.retraitDAO.creerRetraitArticle(pRetrait);
+	}
 }
