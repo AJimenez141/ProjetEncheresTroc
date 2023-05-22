282e552b3641d150fd3401579b6144bd2c2e5bfe
AJimenez141 <102180111+AJimenez141@users.noreply.github.com>
2023-05-22 16:19:42 +0200

    Servlet VendreArticle

--- src/main/java/fr/eni/projet/servlets/ServletVendreUnArticle.java
+++ src/main/java/fr/eni/projet/servlets/ServletVendreUnArticle.java
@@ -1,6 +1,10 @@
 package fr.eni.projet.servlets;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,37 +13,70 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ServletVendreUnArticle
- */
+import fr.eni.projet.bll.ArticleVenduManager;
+import fr.eni.projet.bll.BLLException;
+import fr.eni.projet.bll.CategorieManager;
+import fr.eni.projet.bll.UtilisateurManager;
+import fr.eni.projet.bo.ArticleVendu;
+import fr.eni.projet.bo.Categorie;
+import fr.eni.projet.bo.Utilisateur;
+
 @WebServlet("/VendreUnArticle")
 public class ServletVendreUnArticle extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public ServletVendreUnArticle() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+		List<Categorie> listeCategorie = null;
+		try {
+			listeCategorie = CategorieManager.getInstance().recupererLesCategorie();
+			request.setAttribute("listeCategories",listeCategorie);
+		} catch (BLLException e) {
+			e.printStackTrace();
+		}
 		
 		RequestDispatcher rd = request.getRequestDispatcher("/pages/VendreUnArticle.jsp");
 		rd.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		try
+		{
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			
+			//TODO vendeur en dur pour l'instant. Utiliser variable session id utilisateur pour get
+			Utilisateur vendeur = UtilisateurManager.getInstance().recupererUnUtilisateur(1);
+			
+			int idCategorieChoisie = Integer.parseInt(request.getParameter("selectCategories"));
+			Categorie categorie = CategorieManager.getInstance().recupererUneCategorie(idCategorieChoisie);
+			
+			String nom = request.getParameter("article");
+			String description = request.getParameter("description");
+			
+			LocalDate dateDebut = LocalDate.parse(request.getParameter("debutEnchere"),dtf);
+			LocalDate dateFin = LocalDate.parse(request.getParameter("finEnchere"),dtf);
+			
+			int prixInitial = Integer.parseInt(request.getParameter("prixDepart"));
+			
+			ArticleVendu articleVendu = new ArticleVendu(nom, description, dateDebut, dateFin, prixInitial, vendeur, categorie);
+			
+			ArticleVenduManager.getInstance().ajouterArticleVendu(articleVendu);
+			
+		}
+		catch(DateTimeParseException | BLLException e)
+		{
+			e.printStackTrace();
+		}	
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/Accueil");
+		rd.forward(request, response);
 	}
 
 }
