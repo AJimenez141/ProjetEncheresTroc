37f785206abcc487b28a058060218b6c16b30f6a
Salhigan <loic.huv@gmail.com>
2023-05-23 09:42:28 +0200

    Amélioration du filtre Connexion

--- src/main/java/fr/eni/projet/servlets/FilterConnexion.java
+++ src/main/java/fr/eni/projet/servlets/FilterConnexion.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
@@ -37,28 +38,29 @@ public class FilterConnexion implements Filter {
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		HttpSession session = ((HttpServletRequest) request).getSession();
+		HttpSession session = ((HttpServletRequest) request).getSession(false);
 		
-		if( !httpRequest.getServletPath().toLowerCase().contains("connexion") && !httpRequest.getServletPath().toLowerCase().contains("inscription") && !httpRequest.getServletPath().toLowerCase().contains("accueilnonconnecte"))
+		if( !httpRequest.getServletPath().toLowerCase().contains("connexion") && 
+			!httpRequest.getServletPath().toLowerCase().contains("inscription") && 
+			!httpRequest.getServletPath().toLowerCase().contains("accueilnonconnecte")) //si on accède à une page où la connexion est nécessaire alors
 		{		
-			
-			if(session.getAttribute("pseudo") == null)
+			if(session != null)//si l'utilisateur possède une session, il continue sa navigation
 			{
-				HttpServletResponse httpResponse2 = (HttpServletResponse) response;
-				httpResponse2.sendRedirect("/AccueilNonConnecte");
-			} else {
 				chain.doFilter(httpRequest, response);
+			} else { //sinon on le redirige vers AccueilNonConnecte
+				HttpServletResponse httpResponse = (HttpServletResponse) response;
+				httpResponse.sendRedirect( ( (HttpServletRequest) request).getContextPath() + "/AccueilNonConnecte");
 			}
 		}
-		else
+		else //sinon on accède à une des pages non connecté
 		{
-			if(session.getAttribute("pseudo") != null)
+			if(session != null)//si l'utilisateur possède une session, alors il est redirigé vers Accueil
 			{
 				HttpServletResponse httpResponse = (HttpServletResponse) response;
-				httpResponse.sendRedirect("/Accueil");
+				httpResponse.sendRedirect( ( (HttpServletRequest) request).getContextPath() + "/Accueil");
 			}
-			else {
-				System.out.println("pouet");
+			else { //sinon il continue sa navigation
+				chain.doFilter(httpRequest, response);
 			}
 		}
 	}
