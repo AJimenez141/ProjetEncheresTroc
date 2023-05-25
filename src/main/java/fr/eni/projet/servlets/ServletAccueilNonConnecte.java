c2dd22dde2acd642b09fb8871405d031fffd9e7b
Jérémy LANOUE <jlanoue.18@gmail.com>
2023-05-25 10:08:36 +0200

    correction requete

--- src/main/java/fr/eni/projet/servlets/ServletAccueilNonConnecte.java
+++ src/main/java/fr/eni/projet/servlets/ServletAccueilNonConnecte.java
@@ -43,19 +43,25 @@ public class ServletAccueilNonConnecte extends HttpServlet {
 //		PREPARATION DES VARIABLES
     	List<Enchere> encheres = new ArrayList<>();
     	List<ArticleVendu> articlesVendus = new ArrayList<>();
+    	
 //    	ENCHERES COURANTES OU A DEFAUT ARTICLES EN VENTES
     	List<Enchere> enchereCourantes 	= new ArrayList<>();
     	List<ArticleVendu> articlesEnVente = new ArrayList<>();
+    	
 //    	LISTE CATEGORIES
     	List<Categorie> categories = new ArrayList<>();	
+    	
 //    	INSTANCIATION DES MANAGERS
     	EnchereManager mgr = EnchereManager.getInstance();
     	CategorieManager mgrCat = CategorieManager.getInstance();
     	ArticleVenduManager mgrArt = ArticleVenduManager.getInstance();
+    	
 //    	CREATION D'UNE LISTE D'ERREUR POUR AFFICHAGE
     	List<String> erreurs = new ArrayList<>();
+    	
 //    	RECUPERATION DE LA SESSION
     	HttpSession session = request.getSession();
+    	
     	
 //    	ENCHERES	
 //		RECUPERATION DE TOUS LES ARTICLES
@@ -68,21 +74,23 @@ public class ServletAccueilNonConnecte extends HttpServlet {
 		
 //		AJOUT DES ENCHERES DANS ENCHERESCOURANTES
 		for (ArticleVendu article : articlesVendus) {
-			Enchere plusHauteEnchere = null;
-			int numeroArticle = article.getNoArticle();
-			
-			try {
-				plusHauteEnchere = EnchereManager.getInstance().recupererEnchereLaPlusHaute(numeroArticle);
-			} catch (BLLException e) {
-				e.printStackTrace();
-				erreurs.add(e.toString());
+			if(article.isEnVente()) {		
+				Enchere plusHauteEnchere = null;
+				int numeroArticle = article.getNoArticle();
+				
+				try {
+					plusHauteEnchere = EnchereManager.getInstance().recupererEnchereLaPlusHaute(numeroArticle);
+				} catch (BLLException e) {
+					e.printStackTrace();
+					erreurs.add(e.toString());
+				}
+				
+				if(plusHauteEnchere != null) {
+					enchereCourantes.add(plusHauteEnchere);
+				} else {
+					articlesEnVente.add(article);
+				}	
 			}
-			
-			if(plusHauteEnchere != null) {
-				enchereCourantes.add(plusHauteEnchere);
-			} else {
-				articlesEnVente.add(article);
-			}	
 		}
 
 //    	CATEGORIES
@@ -140,65 +148,74 @@ public class ServletAccueilNonConnecte extends HttpServlet {
     		
 //    		AJOUT DES ENCHERES DANS ENCHERESCOURANTES
     		for (ArticleVendu article : articlesVendus) {
-    			Enchere plusHauteEnchere = null;
-    			int numeroArticle = article.getNoArticle();
-    			
-    			try {
-    				plusHauteEnchere = EnchereManager.getInstance().recupererEnchereLaPlusHaute(numeroArticle);
-    			} catch (BLLException e) {
-    				e.printStackTrace();
-    				erreurs.add(e.toString());
+    			if(article.isEnVente()) {    				
+    				Enchere plusHauteEnchere = null;
+    				int numeroArticle = article.getNoArticle();
+    				
+    				try {
+    					plusHauteEnchere = EnchereManager.getInstance().recupererEnchereLaPlusHaute(numeroArticle);
+    				} catch (BLLException e) {
+    					e.printStackTrace();
+    					erreurs.add(e.toString());
+    				}
+    				
+    				if(plusHauteEnchere != null) {
+    					enchereCourantes.add(plusHauteEnchere);
+    				} else {
+    					articlesEnVente.add(article);
+    				}	
     			}
-    			
-    			if(plusHauteEnchere != null) {
-    				enchereCourantes.add(plusHauteEnchere);
-    			} else {
-    				articlesEnVente.add(article);
-    			}	
     		}
     	} else if(!categorie.equals("Toutes") && !recherche.isBlank()) {
+    		
 			try {
 				encheresFiltrees = ArticleVenduManager.getInstance().recupererLesArticlesVendusParRechercheEtCategorie(recherche,categorie);
 			} catch (BLLException e) {
 				e.printStackTrace();
 				erreurs.add(e.toString());
 			}
+			
 		} else if(!recherche.equals("")) {
+			
 			try {
 				encheresFiltrees = ArticleVenduManager.getInstance().recuperLesArticlesVendusParRecherche(recherche);
-				for (ArticleVendu art : encheresFiltrees) {
-					System.out.println(art);
-				}
 			} catch (BLLException e) {
 				e.printStackTrace();
 				erreurs.add(e.toString());
 			}	
-		} else if(!categorie.equals("Toutes")) {	
+			
+		} else if(!categorie.equals("Toutes")) {
+			
 			try {
 				encheresFiltrees = ArticleVenduManager.getInstance().recupererLesArticlesVendusParCategorie(categorie);
 			} catch (BLLException e) {
 				e.printStackTrace();
 				erreurs.add(e.toString());
 			}
+			
 		}
 		
-    	if(!encheresFiltrees.isEmpty()) { 		
+    	if(!encheresFiltrees.isEmpty()) { 	
+    		
     		for (ArticleVendu article : encheresFiltrees) {
-    			Enchere plusHauteEnchere = null;
-    			int numeroArticle = article.getNoArticle();
     			
-    			try {
-    				plusHauteEnchere = EnchereManager.getInstance().recupererEnchereLaPlusHaute(numeroArticle);
-    			} catch (BLLException e) {
-    				e.printStackTrace();
-    				erreurs.add(e.toString());
+    			if(article.isEnVente()) {
+    				Enchere plusHauteEnchere = null;
+    				int numeroArticle = article.getNoArticle();
+    				
+    				try {
+    					plusHauteEnchere = EnchereManager.getInstance().recupererEnchereLaPlusHaute(numeroArticle);
+    				} catch (BLLException e) {
+    					e.printStackTrace();
+    					erreurs.add(e.toString());
+    				}
+    				
+    				if(plusHauteEnchere != null) {
+    					enchereCourantes.add(plusHauteEnchere);
+    				} else {
+    					articlesEnVente.add(article);
+    				}	
     			}
-    			
-    			if(plusHauteEnchere != null) {
-    				enchereCourantes.add(plusHauteEnchere);
-    			} else {
-    				articlesEnVente.add(article);
-    			}	
     		}
 		}
 		
